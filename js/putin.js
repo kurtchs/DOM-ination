e20b23c7ee16b9faa82778478e549f23bf7846e9
“Kurt <chaconsaavedrakurt@gmailcomgit config --global user.name Kurt Chacongit config --global user.email chaconsaavedrakurt@gmailcom>
2025-02-21 11:15:35 +0100

    añadidas nuevas imagenes

---
+++ js/putin.js
@@ -0,0 +1,36 @@
+class Putin {
+    constructor(type, positionY){
+
+        this.node = document.createElement("img")
+        
+
+        this.node.src = "./img/putin.png"
+      
+        gameBoxNode.append(this.node)
+
+
+        this.x = gameBoxNode.offsetWidth;
+        this.y = positionY;
+        this.w = 100;
+        this.h = 90;
+
+        this.node.style.position ="absolute"
+        this.node.style.left = `${this.x}px`
+        this.node.style.top = `${this.y}px`
+        this.node.style.width = `${this.w}px`
+        this.node.style.height = `${this.h}px`
+
+        this.movementSpeed = 2
+
+        this.type = type
+
+    }
+
+    movement() {
+        this.x -= this.movementSpeed
+        this.node.style.left = `${this.x}px`
+
+    }
+
+
+}
